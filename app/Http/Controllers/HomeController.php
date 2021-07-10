016a3eaa8290d9ca85557faf3bb8a624469bd560
mastrmind-dev <asapthaka@gmail.com>
2021-07-10 09:15:14 +0530

    Task Card

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\NewTask;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $task=NewTask::orderBy('date')->get();
+        //$item_where=Item::where('type','gg')->get();
+        //get all the items
+        //$items = Item::all();
+        //$items_latest=Item:latest();
+        return View('home')->with('tasks',$task);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,89 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card text-white bg-success">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{ __('You are logged in!') }}
+                        {{ __('You are logged in!') }}
+                    </div>
                 </div>
+
+
+
             </div>
         </div>
+
+        @foreach ($tasks as $task)
+            <div class="row justify-content-center mt-5">
+                <div class="col-md-8">
+                    <div class="card border-success" style="border-radius: 30pt">
+                        <div class="card-header bg-success" style="border-top-left-radius: 30pt; border-top-right-radius: 30pt;">
+                            <div class="row">
+                                <div class="col-md">
+                                    <img src="..." alt="..." class="img-thumbnail">
+                                </div>
+                                <div class="col-md-6 float-left text-light">
+                                    <h5 class="font-weight-bold">Assignment {{ $task->id }}</h5>
+                                    <h6 class="font-weight-bold">Course {{ $task->course }}</h6>
+                                </div>
+                                <div class="col-md-4 text-right text-light">
+                                    <h5 class="font-weight-bold">{{ $task->date }}</h5>
+                                    <h6 class="font-weight-bold">{{ $task->time }}</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <img src="">
+                            <h5 class="card-title font-weight-bold">{{ $task->title }}</h5>
+                            <p class="card-text" aria-placeholder="Description">{{ $task->content }}</p>
+                            <a href="#" class="btn btn-primary float-right bg-success">Mark as Done</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+
+        {{-- @foreach ($tasks as $task)
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card text-white bg-success boder-dark" style="border-radius: 30pt; border-color: rgb(0, 128, 0);">
+                        <div class="card-header" style="border-top-left-radius: 30pt; border-top-right-radius: 30pt;">
+                            <div class="row">
+                                <div class="col-md">
+                                    <img src="..." alt="..." class="img-thumbnail">
+                                </div>
+                                <div class="col-md-6 float-left text-light">
+                                    <h5 class="font-weight-bold">Assignment {{ $task->id }}</h5>
+                                    <h6 class="font-weight-bold">Course {{ $task->course }}</h6>
+                                </div>
+                                <div class="col-md-4 text-right text-light">
+                                    <h5 class="font-weight-bold">{{ $task->date }}</h5>
+                                    <h6 class="font-weight-bold">{{ $task->time }}</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <img src="">
+                            <h5 class="card-title font-weight-bold">{{ $task->title }}</h5>
+                            <p class="card-text" aria-placeholder="Description">{{ $task->content }}</p>
+                            <a href="#" class="btn btn-primary float-right bg-success">Mark as Done</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach --}}
+        <p class="task_created">{{ session('message') }}</p>
+
     </div>
-</div>
+    </div>
 @endsection
