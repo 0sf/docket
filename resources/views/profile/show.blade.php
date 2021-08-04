e42d146b384bc9b444af5f3eb375092a2eb2e9ea
HiruniSenevirathne <senevirathnehu@gmail.com>
2021-08-04 20:14:37 +0530

    profile.show picture size changed

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -14,7 +14,7 @@
         <div class="col-lg-3">
             @if (Auth::user()->image)
 
-            <img src="{{('/upload/profiles/'.Auth::user()->image)}}" class="rounded-circle mt-5" width="90%" style=" left:25%;  top:15%">
+            <img src="{{('/upload/profiles/'.Auth::user()->image)}}" class="rounded-circle mt-5" width="350" style=" left:25%;  top:15%">
 
             @else
 
